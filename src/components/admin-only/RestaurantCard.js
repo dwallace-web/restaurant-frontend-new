311dacfb90647802be4d71be49240cd4fe3e06b0
David Wallace <dwallacemarketing@gmail.com>
2021-01-21 00:53:53 -0500

    working edit rest function CRUD

--- src/components/admin-only/RestaurantCard.js
+++ src/components/admin-only/RestaurantCard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Collapse, Button } from 'reactstrap';
+import editRestaurant from './editRestaurant';
 
 export class RestaurantCard extends Component {
   constructor(props) {
@@ -14,6 +15,43 @@ export class RestaurantCard extends Component {
     this.setState((prevState) => ({
       isOpen: !prevState.isOpen,
     }));
+  };
+
+  editRestaurant = (e) => {
+    e.preventDefault();
+    // console.log('works');
+
+    const input = {
+      name: this.name,
+      address: this.address,
+      phonenumber: this.phonenumber,
+      category: this.category,
+    };
+
+    console.log(
+      'test input -->',
+      input,
+      'fetch url--->',
+      `http://localhost:2000/restaurant/${this.props.restaurant.id}`
+    );
+
+    try {
+      // const input = this.state;
+      fetch(`http://localhost:2000/restaurant/${this.props.restaurant.id}`, {
+        method: 'PUT',
+        headers: new Headers({
+          'Content-Type': 'application/json',
+          Authorization: this.props.token,
+        }),
+        body: JSON.stringify(input),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+        });
+    } catch (error) {
+      console.log('error', error);
+    }
   };
 
   render() {
@@ -40,7 +78,40 @@ export class RestaurantCard extends Component {
             Delete
           </Button>
           <Collapse isOpen={this.state.isOpen}>
-            <h1>Hello World!</h1>
+            Hello World
+            {/* <editRestaurant />
+            Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference. Learn more about using refs safely here: https://reactjs.org/link/strict-mode-find-node
+            */}
+            <form onSubmit={this.editRestaurant}>
+              <input
+                placeholder="Name"
+                type="text"
+                name="name"
+                required
+                onChange={(e) => (this.name = e.target.value)}
+              />
+              <input
+                placeholder="Address"
+                type="text"
+                name="address"
+                required
+                onChange={(e) => (this.address = e.target.value)}
+              />
+              <input
+                placeholder="phone number"
+                type="text"
+                name="phonenumber"
+                onChange={(e) => (this.phonenumber = e.target.value)}
+              />
+              <input
+                placeholder="category"
+                type="text"
+                name="category"
+                required
+                onChange={(e) => (this.category = e.target.value)}
+              />
+              <button type="submit"> Submit</button>
+            </form>
           </Collapse>
         </div>
       </div>
---
+++ src/components/admin-only/editRestaurant.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class editRestaurant extends Component {
+  render() {
+    return (
+      <div>
+        <h2>EDit restaurnt form</h2>
+      </div>
+    );
+  }
+}
+
+export default editRestaurant;
