f2470cac662d3cf881e83dac2352da7c3e3422bb
David Wallace <David@David-Macbook-Air.local>
2022-09-12 21:18:47 -0500

    Updates

---
+++ precache-manifest.c8e377ef9168bdfb0e527bdd7ba0ac99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ebd1e8992114473dd19f",
+    "url": "/restaurant-frontend-new/static/css/main.7e097211.chunk.css"
+  },
+  {
+    "revision": "ebd1e8992114473dd19f",
+    "url": "/restaurant-frontend-new/static/js/main.ebd1e899.chunk.js"
+  },
+  {
+    "revision": "37e0b73d954d18ff3ca9",
+    "url": "/restaurant-frontend-new/static/js/1.37e0b73d.chunk.js"
+  },
+  {
+    "revision": "cb08177b8671c93bc782",
+    "url": "/restaurant-frontend-new/static/js/2.cb08177b.chunk.js"
+  },
+  {
+    "revision": "1843d9781a7eb9cffb01",
+    "url": "/restaurant-frontend-new/static/js/runtime~main.1843d978.js"
+  },
+  {
+    "revision": "d3ea8c5216ec5846e731c253574b96db",
+    "url": "/restaurant-frontend-new/index.html"
+  }
+];
